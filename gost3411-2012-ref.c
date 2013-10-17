ac5f2e8c436facfcf9571f26dc393bf0abbb2708
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-10-17 18:05:03 +0300

    Fix typo in bswap64()

--- gost3411-2012-ref.c
+++ gost3411-2012-ref.c
@@ -15,7 +15,7 @@ static inline uint64_t bswap64(uint64_t x)
 		((x & 0x00000000FF000000ULL) <<  8) |
 		((x & 0x0000000000FF0000ULL) << 24) |
 		((x & 0x000000000000FF00ULL) << 40) |
-		((x & 0x00000000000000FFULL) << 56))
+		((x & 0x00000000000000FFULL) << 56)
 	;
 #endif
 }
